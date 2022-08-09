27e6a6765ce565560a81b9763c59fbde375a0cf0
vladadriann <vlad.oprea@outlook.com>
2022-08-09 17:20:02 +0300

    created custom inputs for sign in

--- mobile/src/screens/SignInScreen/SignInScreen.js
+++ mobile/src/screens/SignInScreen/SignInScreen.js
@@ -1,12 +1,46 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, Image, StyleSheet, useWindowDimensions} from 'react-native';
+import React, {useState} from 'react';
+import Logo from '../../../assets/images/Logo_1.png';
+import CustomInput from '../../components/CustomInput';
 
-const SIgnInScreen = () => {
+const SignInScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const {height} = useWindowDimensions();
   return (
-    <View>
-      <Text>SIgnInScreen</Text>
+    <View style={styles.root}>
+      <Image
+        source={Logo}
+        style={(styles.logo, {height: height * 0.3})}
+        resizeMode="contain"
+      />
+
+      <CustomInput
+        placeholder="Username"
+        value={username}
+        setValue={setUsername}
+      />
+      <CustomInput
+        placeholder="Password"
+        value={password}
+        setValue={setPassword}
+        secureTextEntry={true}
+      />
     </View>
   );
 };
 
-export default SIgnInScreen;
+const styles = StyleSheet.create({
+  root: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: '70%',
+    maxWidth: 300,
+    maxHeight: 200,
+  },
+});
+
+export default SignInScreen;
