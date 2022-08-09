61f04756b3dcba208a3563b1b7d145901b43e878
vladadriann <vlad.oprea@outlook.com>
2022-08-09 16:17:24 +0300

    setup for mobile dev

---
+++ mobile/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import SignInScreen from './src/screens/SignInScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.root}>
+      <SignInScreen />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ mobile/src/screens/SignInScreen/SignInScreen.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const SIgnInScreen = () => {
+  return (
+    <View>
+      <Text>SIgnInScreen</Text>
+    </View>
+  );
+};
+
+export default SIgnInScreen;
